f8a0a059eb55e06b51335929705a3ffaedcb538c
Alexander Quaade Sørensen <Larsquaadeconnect@gmail.com>
2023-12-28 04:56:03 +0100

    My memory says I tried to make a smarter sphere here?

--- VolatileFlameProject/main.cpp
+++ VolatileFlameProject/main.cpp
@@ -71,6 +71,7 @@ void setMaterialUniforms();
 void setLightUniforms();
 unsigned int loadTexture(const char* filepath);
 void FPSUpdate();
+std::vector<float> buildUnitPositiveX(int subdivision);
 
 // global variables we will use to store our objects, shaders, and active shader
 const unsigned int SCR_WIDTH = 800;
@@ -239,6 +240,17 @@ int main()
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		GLint maxLength = 0;
+
+		glGetShaderiv(shader->ID, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(shader->ID, maxLength, &maxLength, &errorLog[0]);
+
+		// Provide the infolog in whatever manner you deem best.
+		// The errorLog vector contains the log message as a C string.
+		std::cerr << &errorLog[0] << std::endl;
 	}
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
@@ -589,4 +601,63 @@ void FPSUpdate()
 	int FPS = (1.0 / deltaTime);
 	std::string newTitle = windowName + std::to_string(FPS) + " FPS / " + std::to_string(deltaTime) + " ms";
 	glfwSetWindowTitle(window, newTitle.c_str());
+}
+
+// generate vertices for +X face only by intersecting 2 circular planes
+// (longitudinal and latitudinal) at the given longitude/latitude angles
+std::vector<float> buildUnitPositiveX(int subdivision)
+{
+	const float DEG2RAD = acos(-1) / 180.0f;
+
+	std::vector<float> vertices;
+	float n1[3];        // normal of longitudinal plane rotating along Y-axis
+	float n2[3];        // normal of latitudinal plane rotating along Z-axis
+	float v[3];         // direction vector intersecting 2 planes, n1 x n2
+	float a1;           // longitudinal angle along Y-axis
+	float a2;           // latitudinal angle along Z-axis
+
+	// compute the number of vertices per row, 2^n + 1
+	int pointsPerRow = (int)pow(2, subdivision) + 1;
+
+	// rotate latitudinal plane from 45 to -45 degrees along Z-axis (top-to-bottom)
+	for (unsigned int i = 0; i < pointsPerRow; ++i)
+	{
+		// normal for latitudinal plane
+		// if latitude angle is 0, then normal vector of latitude plane is n2=(0,1,0)
+		// therefore, it is rotating (0,1,0) vector by latitude angle a2
+		a2 = DEG2RAD * (45.0f - 90.0f * i / (pointsPerRow - 1));
+		n2[0] = -sin(a2);
+		n2[1] = cos(a2);
+		n2[2] = 0;
+
+		// rotate longitudinal plane from -45 to 45 along Y-axis (left-to-right)
+		for (unsigned int j = 0; j < pointsPerRow; ++j)
+		{
+			// normal for longitudinal plane
+			// if longitude angle is 0, then normal vector of longitude is n1=(0,0,-1)
+			// therefore, it is rotating (0,0,-1) vector by longitude angle a1
+			a1 = DEG2RAD * (-45.0f + 90.0f * j / (pointsPerRow - 1));
+			n1[0] = -sin(a1);
+			n1[1] = 0;
+			n1[2] = -cos(a1);
+
+			// find direction vector of intersected line, n1 x n2
+			v[0] = n1[1] * n2[2] - n1[2] * n2[1];
+			v[1] = n1[2] * n2[0] - n1[0] * n2[2];
+			v[2] = n1[0] * n2[1] - n1[1] * n2[0];
+
+			// normalize direction vector
+			float scale = 1 / sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+			v[0] *= scale;
+			v[1] *= scale;
+			v[2] *= scale;
+
+			// add a vertex into array
+			vertices.push_back(v[0]);
+			vertices.push_back(v[1]);
+			vertices.push_back(v[2]);
+		}
+	}
+
+	return vertices;
 }
